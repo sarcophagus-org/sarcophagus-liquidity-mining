a8dea173cca466272d0390ce4082cb0514e37d1d
Adam Gall <adam@decentlabs.io>
2020-11-30 15:27:42 -0500

    Remove default CRA stuff

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <header>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
