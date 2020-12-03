9c03ea8913f2cba64b24f82c6d2c9c8f797c0728
Adam Gall <adam@decentlabs.io>
2020-12-03 09:52:16 -0500

    Set up truffle for testnet deployment

--- migrations/3_deposit_reward.js
+++ migrations/3_deposit_reward.js
@@ -5,14 +5,23 @@ const SarcoMock = artifacts.require("SarcoMock")
 const BN = web3.utils.BN
 
 module.exports = async function (_, network) {
-  if (!["mainnet", "mainnet-fork"].includes(network)) {
+  if (["development", "test"].includes(network)) {
     const liquidityMining = await LiquidityMining.deployed()    
-    const amount = (new BN(500000)).mul(new BN(10).pow(new BN(18)))
+    const amount = (new BN(1000000)).mul(new BN(10).pow(new BN(18)))
 
     const sarcoMock = await SarcoMock.deployed()
     await sarcoMock.approve(liquidityMining.address, amount)
 
-    const blockLength = 1000
-    await liquidityMining.deposit(amount, (await web3.eth.getBlock("latest")).number + 10, blockLength)
+    const blockLength = 10
+    await liquidityMining.deposit(amount, (await web3.eth.getBlock("latest")).number + 8, blockLength)
+  } else if (["goerli"].includes(network)) {
+    const liquidityMining = await LiquidityMining.deployed()    
+    const amount = (new BN(1000000)).mul(new BN(10).pow(new BN(18)))
+
+    const sarcoMock = await SarcoMock.deployed()
+    await sarcoMock.approve(liquidityMining.address, amount)
+
+    const blockLength = 50000
+    await liquidityMining.deposit(amount, (await web3.eth.getBlock("latest")).number + 100, blockLength)
   }
 }
