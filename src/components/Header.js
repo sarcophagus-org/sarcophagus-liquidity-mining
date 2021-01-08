2bf89f4fbcf7a0bcced9bf55bfae93a64be9006f
Adam Gall <adam@decentlabs.io>
2021-01-07 20:59:06 -0500

    Add API keys to getDefaultProvider

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { useData } from '../dataContext'
 import { useWeb3 } from '../web3'
 import logo from '../assets/images/logo.png'
 import wallet from '../assets/images/wallet.svg'
-import { useUserSuppliedConnect, connect } from '../web3/userSupplied'
+import { connect } from '../web3/userSupplied'
 
 const AccountDisplay = () => {
   const truncate = (fullStr, strLen, separator) => {
