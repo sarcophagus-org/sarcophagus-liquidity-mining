78bf59f46a9b7c8445aff3802b365e80b4e2f2a9
Adam Gall <adam@decentlabs.io>
2021-05-04 15:37:35 -0400

    Update an environment variable in code

--- src/web3/fallback.js
+++ src/web3/fallback.js
@@ -3,7 +3,7 @@ import { getDefaultProvider } from 'ethers'
 
 const useFallbackConnect = (previousProvider) => {
   const [provider, setProvider] = useState(null)
-  const [defaultProvider] = useState(getDefaultProvider(parseInt(process.env.REACT_APP_DEFAULT_CHAIN_ID, 10), {
+  const [defaultProvider] = useState(getDefaultProvider(parseInt(process.env.REACT_APP_CHAINID, 10), {
     alchemy: 'bjrgvDbGZBUJ_iZFFd1vIqUWzgrwKWeg',
     etherscan: 'G61W8715WK65NCMJPFXU8EBQ5VDTT4KCVJ',
     infura: '800746c8737e4f34bb772ade4532ab83',
