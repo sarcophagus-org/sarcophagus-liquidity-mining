479f259a6700ab12aacdecce09ecb2c86940f332
Adam Gall <adam@decentlabs.io>
2021-05-12 11:13:02 -0400

    Show 0 seconds

--- src/dataContext/index.js
+++ src/dataContext/index.js
@@ -90,7 +90,7 @@ const createDataRoot = () => {
     var dDisplay = d > 0 ? d + (d === 1 ? " day, " : " days, ") : ""
     var hDisplay = h > 0 ? h + (h === 1 ? " hour, " : " hours, ") : ""
     var mDisplay = m > 0 ? m + (m === 1 ? " minute, " : " minutes, ") : ""
-    var sDisplay = s > 0 ? s + (s === 1 ? " second" : " seconds") : ""
+    var sDisplay = s + (s === 1 ? " second" : " seconds")
 
     if (!dDisplay && !hDisplay && !mDisplay && !sDisplay) sDisplay = "0 seconds"
 
